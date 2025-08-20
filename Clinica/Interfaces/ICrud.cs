6689a7ce77b2e10b9ebb3a4e2c887523db7ff108
Santiago Maya Horta <santymaya9@gmail.com>
2025-08-19 23:35:37 -0500

    Ya elimine las clase inncesarias

--- Clinica/Interfaces/ICrud.cs
+++
@@ -1,5 +0,0 @@
-public interface ICrud
-{
-    void Agregar(uint id, string nombre, string correo, int celular, string tipo_cedula, int cedula, string contrasena, IRegistrar tipo_registro);
-    
-}
